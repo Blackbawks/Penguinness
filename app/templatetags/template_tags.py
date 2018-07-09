d58d0bce942a68c6c460baf190241e78fc92427d
black bawks data science <blackbawks.datascience@gmail.com>
2018-07-10 00:42:52 +0100

    Fixed issues 1, 7, 8 and 9

--- app/templatetags/template_tags.py
+++ app/templatetags/template_tags.py
@@ -8,7 +8,14 @@ register = template.Library()
 def settings_value(name):
     return getattr(settings, name, "")
 
-
 @register.assignment_tag
 def query(qs, **kwargs):
     return qs.filter(**kwargs)
+
+@register.filter
+def convertblank(val):
+    if(val == 0):
+        return ''
+    else:
+        return val
+
